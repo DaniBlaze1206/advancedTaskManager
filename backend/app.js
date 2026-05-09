403dc655356755ebcdb21d212b3b2f2b18bf47d0
Daniel <danielghannadi1206@gmail.com>
2026-05-10 00:07:00 +0330

    feat: implement register api

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,13 @@ require('dotenv').config();
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
 const PORT = process.env.PORT;
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded());
 
-
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
 	console.log(`server is running on port ${PORT}`);
