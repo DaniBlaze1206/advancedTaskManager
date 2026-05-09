996d6d4f34faeb9eeb3aa1d8dc4a5abf679618ab
Daniel <danielghannadi1206@gmail.com>
2026-05-09 23:28:09 +0330

    feat: add user model

---
+++ backend/src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  { timestamps: true },
+);
+
+
+module.exports = mongoose.model("User", userSchema)
