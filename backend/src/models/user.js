780dfd9f5aaf8f79b88d7e6151b053dd67d75f72
Daniel <danielghannadi1206@gmail.com>
2026-06-02 15:31:16 +0330

    fix: export user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -24,4 +24,4 @@ const userSchema = new mongoose.Schema(
 );
 
 
-mongoose.models.User || mongoose.model("User", userSchema)
+module.exports = mongoose.models.User || mongoose.model("User", userSchema)
